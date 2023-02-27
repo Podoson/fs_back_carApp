ef51980a2e0b2256fed534c2a8039c1cf80d2b35
podopapson4 <podopapson4@gmail.com>
2023-02-27 18:00:57 +0100

    version 1

--- src/main/java/com/podosoft/fullstack/web/CarController.java
+++ src/main/java/com/podosoft/fullstack/web/CarController.java
@@ -3,9 +3,11 @@ package com.podosoft.fullstack.web;
 import com.podosoft.fullstack.domain.Car;
 import com.podosoft.fullstack.domain.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class CarController {
 
