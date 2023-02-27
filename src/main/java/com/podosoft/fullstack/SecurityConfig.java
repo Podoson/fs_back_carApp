40ec55e9660285c99bbcfb96605c016aacff6c98
podopapson4 <podopapson4@gmail.com>
2023-02-27 21:56:18 +0100

    version 1

--- src/main/java/com/podosoft/fullstack/SecurityConfig.java
+++ src/main/java/com/podosoft/fullstack/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable().cors().and()
+        http.csrf().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
