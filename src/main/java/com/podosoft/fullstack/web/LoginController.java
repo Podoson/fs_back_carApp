51af58e152a8ee3d6cd4d51b30c9f9b85c1909c5
podopapson4 <podopapson4@gmail.com>
2023-02-28 20:43:19 +0100

    version 1

--- src/main/java/com/podosoft/fullstack/web/LoginController.java
+++ src/main/java/com/podosoft/fullstack/web/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LoginController {
     @Autowired
     private JwtService jwtService;
